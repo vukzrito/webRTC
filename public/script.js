5df2cb269f53166ad999d9b3ec71d6e4e37efab6
Rito Vukela <vukzrito@gmail.com>
2021-10-30 01:28:15 +0200

    Send stream to newly connected user

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
     host: '/',
-    port: 3001, debug:2
+    port: 3001, debug: 2
 })
 
 const myVideo = document.createElement('video')
@@ -15,11 +15,16 @@ navigator.mediaDevices.getUserMedia({
 
     myPeer.on('call', call => {
         console.log('call requesting stream received')
-       call.answer(stream)
-       console.log('stream sent')
-
+        call.answer(stream)
+        console.log('stream sent')
+        
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            console.log('Stream received from user')
+            setTimeout(addVideoStream(video, userVideoStream), 10000)
+        })
     })
-    
+
 
     socket.on('user-connected', (userId) => {
         console.log('User connected: ', userId);
@@ -28,7 +33,7 @@ navigator.mediaDevices.getUserMedia({
 })
 socket.on('user-disconnected', userId => {
     console.log(`User left room`)
-   
+
 })
 
 myPeer.on('open', id => {
@@ -45,8 +50,9 @@ function connectToNewUser(userId, stream) {
 
     call.on('stream', userVideoStream => {
         console.log('Stream received from user')
-        setTimeout(addVideoStream(video, userVideoStream), 3000)
+        setTimeout(addVideoStream(video, userVideoStream), 10000)
     })
+    call.on('error', error => console.error(error))
 
     call.on('close', () => {
         console.log('log connection closed')
