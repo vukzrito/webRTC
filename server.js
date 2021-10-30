f20dcfbbedbc739a5f0116680749872d2d2a98f9
Rito Vukela <vukzrito@gmail.com>
2021-10-30 13:34:20 +0200

    Fix script references

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidv4 } = require('uuid');
+const { PeerServer } = require('peer');
+
+const peerServer = PeerServer({ port: 3001 });
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
